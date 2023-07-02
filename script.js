704cf3c5de3b9827173107e2745936940ad5e5bc
Tanzil Pasha <84717048+tanzil7786@users.noreply.github.com>
2023-07-02 18:43:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded" , function(){
-	var body =  document.querySelector("body");
-	if(body.hasChildNodes()){
-		body.innerHtml="";
-	}
-	var textNode = document.createTextNode("DOM load success");
-	body.appendChild(textNode);
+window.addEventListener('load' , () =>{
+	document.getElementsByTagName('body')[0].innerText="DOM load success";
 })
